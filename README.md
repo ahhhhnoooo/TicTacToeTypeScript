f48a98fe852db40da21e11d9936a27ab355f5ae4
Ben Ng <ahhhhnoooo@gmail.com>
2022-08-06 19:01:46 -0700

    BKN First pass, not much usage in TypeScript...

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # TicTacToeTypeScript
+A quick example of TicTacToe game in HTML+CSS+TypeScript
+
+Build it with 
+```
+tsc
+```
---
+++ index.ts
@@ -0,0 +1,90 @@
+class Game {
+
+    board: string[];
+    winner: string;
+    turn: string;
+
+    init = () => {
+        this.board = [];
+        this.winner = '';
+        this.turn = 'x';
+    }
+
+    onClick = (index: number) => {
+        if(this.winner) return;
+        this.board[index] = this.turn;
+
+        console.log(this.board)
+        if (this.checkWinner()) {
+            this.winner = this.turn;
+        }
+        else {
+            this.nextTurn();
+        }
+    }
+
+    //Manually checking for the winner
+    checkWinner = () => {
+        if (
+            (this.turn === this.board[0] && this.turn === this.board[1] && this.turn === this.board[2]) ||
+            (this.turn === this.board[3] && this.turn === this.board[4] && this.turn === this.board[5]) ||
+            (this.turn === this.board[6] && this.turn === this.board[7] && this.turn === this.board[8]) ||
+            (this.turn === this.board[0] && this.turn === this.board[3] && this.turn === this.board[6]) ||
+            (this.turn === this.board[1] && this.turn === this.board[4] && this.turn === this.board[7]) ||
+            (this.turn === this.board[2] && this.turn === this.board[5] && this.turn === this.board[8]) ||
+            (this.turn === this.board[0] && this.turn === this.board[4] && this.turn === this.board[8]) ||
+            (this.turn === this.board[2] && this.turn === this.board[4] && this.turn === this.board[6])) {
+            console.log(this.turn, "WINS")
+                return true;
+        } else { return false; }
+
+    }
+
+    //Change to the other player's turn
+    nextTurn = () => {
+        this.turn = (this.turn === 'x') ? 'o' : 'x';
+    }
+}
+
+class HTMLRenderer {
+
+    game;
+    spaces;
+
+    onClickSpace = (index) => {
+        this.spaces[index].innerText = this.game.turn;
+        this.game.onClick(index); 
+        if(this.game.winner){
+            let gameElement = document.getElementById("game");
+            let gameEndElement = document.createElement("div");
+            gameEndElement.className = "end";
+            gameEndElement.innerText = this.game.winner + " WINS";
+            gameElement.appendChild(gameEndElement);
+        }
+    }
+
+    init = (gameObj) => {
+        this.spaces = [];
+        this.game = gameObj;
+        let gameElement = document.getElementById("game");
+        let gameRowElement, gameCellElement;
+        for (let index = 0; index < 9; ++index) {
+            if (index % 3 === 0) {
+                gameRowElement = document.createElement("div");
+                gameRowElement.className = "row";
+                gameElement?.appendChild(gameRowElement);
+            }
+            gameCellElement = document.createElement("div");
+            gameCellElement.className = "cell";
+            gameRowElement.appendChild(gameCellElement);
+            this.spaces.push(gameCellElement)
+            gameCellElement.onclick = (() => this.onClickSpace(index));
+        }
+
+    }
+}
+
+let game = new Game();
+game.init();
+let renderer = new HTMLRenderer();
+renderer.init(game);
