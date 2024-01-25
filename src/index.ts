181add11f4196731ad1b321bddfcfef066993e0e
Ben Ng <benng@Bens-Mac-mini.lan>
2024-01-24 19:46:14 -0800

    Fix build/package issues

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-export class TicTacToeGameState {
+class TicTacToeGameState {
     /* 
         The board is laid out in rows of 3
         0,1,2
@@ -12,7 +12,7 @@ export class TicTacToeGameState {
     /** The current player: 'x' or 'o' */
     turn: string;
 }
-export class TicTacToeGame {
+class TicTacToeGame {
     static onClick = (game, index): TicTacToeGameState => {
         let result: TicTacToeGameState = game;
         if (game.winner === '') {
@@ -66,3 +66,38 @@ export class TicTacToeGame {
         }
     }
 }
+class HTMLRenderer {
+    game = new TicTacToeGameState();
+
+    onClickSpace = (index, element) => {
+        element.innerText = this.game.turn;
+        this.game = TicTacToeGame.onClick(this.game, index); 
+        if(this.game.winner){
+            let gameElement = document.getElementById("game");
+            let gameEndElement = document.createElement("div");
+            gameEndElement.className = "end";
+            gameEndElement.innerText = this.game.winner + " WINS";
+            gameElement.appendChild(gameEndElement);
+        }
+    }
+
+    init = () => {
+        this.game = TicTacToeGame.init();
+        let gameElement = document.getElementById("game");
+        let gameRowElement;
+        for (let index = 0; index < 9; ++index) {
+            if (index % 3 === 0) {
+                gameRowElement = document.createElement("div");
+                gameRowElement.className = "row";
+                gameElement?.appendChild(gameRowElement);
+            }
+            let gameCellElement = document.createElement("div");
+            gameCellElement.className = "cell";
+            gameRowElement.appendChild(gameCellElement);
+            gameCellElement.onclick = (() => this.onClickSpace(index, gameCellElement));
+        }
+    }
+}
+
+let renderer = new HTMLRenderer();
+renderer.init();
